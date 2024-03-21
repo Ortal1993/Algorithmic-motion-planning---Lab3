83ff72cecda4288c8265541a649ebfb00e6228f2
Ortal1993 <cohen1993@gmail.com>
2024-03-21 19:01:19 +0200

    first commit - only paste from lab2

--- building_blocks.py
+++ building_blocks.py
@@ -1,4 +1,11 @@
 import numpy as np
+from scipy.spatial.distance import cdist
+from visualizer import Visualize_UR
+
+X = 0
+Y = 1
+Z = 2
+MIN_RESOLUTION = 3
 
 class Building_Blocks(object):
     '''
@@ -13,29 +20,83 @@ class Building_Blocks(object):
         self.p_bias = p_bias
         self.cost_weights = np.array([0.4, 0.3 ,0.2 ,0.1 ,0.07 ,0.05])
         
+        self.checked_confs = {} #dict[conf] = bool #dict[conf] = bool, true if there is a collision and false otherwise
+
+        self.possible_joints_collision = []
+        for i in range(len(self.ur_params.ur_links)):
+            #The 3 upper parts can't be in collision
+            if self.ur_params.ur_links == 'wrist_1_link':
+                break
+            for j in range(i + 2, len(self.ur_params.ur_links)):
+                self.possible_joints_collision.append((self.ur_params.ur_links[i], self.ur_params.ur_links[j]))
+        
     def sample(self, goal_conf) -> np.array:
         """
         sample random configuration
         @param goal_conf - the goal configuration
         """
-        # TODO 
-        # hint - use self.ur_params.mechamical_limits
-        
-        # return np.array(conf)
-        
-
+        #samples a value for the joint. If the random number is less than p_bias, 
+        #it samples from the goal configuration for that joint. 
+        #Otherwise, it samples randomly within the joint limits.    
+        if np.random.uniform(0, 1) < self.p_bias:
+            return goal_conf
+        # With probability 1 - p_bias, sample randomly within joint limits
+        conf = []
+        for _, joint_limits in self.ur_params.mechamical_limits.items():            
+            lower_limit, upper_limit = joint_limits[0], joint_limits[1]
+            conf.append(np.random.uniform(lower_limit, upper_limit))
+        return np.array(conf)
+           
     def is_in_collision(self, conf) -> bool:
-        """check for collision in given configuration, arm-arm and arm-obstacle
+        """
+        check for collision in given configuration, arm-arm and arm-obstacle
         return True if in collision
         @param conf - some configuration 
         """
-        # TODO 
-        # hint: use self.transform.conf2sphere_coords(), self.ur_params.sphere_radius, self.env.obstacles
+        #visualizer = Visualize_UR(self.ur_params, self.env, self.transform, self)
+        #visualizer.show_conf(conf)
+
+        tupl_conf = tuple(conf)
+        try:
+            res = self.checked_confs[tupl_conf]
+            return res
+        except KeyError:
+            #if it was not checked already - set the default value as True 
+            self.checked_confs[tupl_conf] = True
+        
         global_sphere_coords = self.transform.conf2sphere_coords(conf)
+
+        sphere_radius_per_joint = self.ur_params.sphere_radius
+
+        # arm - floor/window collision
+        for joint, spheres in global_sphere_coords.items():
+            if joint == 'shoulder_link':
+                    continue
+            if any (((sphere[2] - sphere_radius_per_joint[joint]) <= 0) or (sphere[0] + sphere_radius_per_joint[joint] > 0.4) for sphere in spheres):
+                return True
+        
         # arm - arm collision
-        
-        # arm - obstacle   
-        
+        spheres_per_joint = {joint: np.array(global_sphere_coords[joint]) for joint in self.ur_params.ur_links}
+        for joint_1, joint_2 in self.possible_joints_collision:
+            robot_spheres_1 = [spheres for spheres in spheres_per_joint[joint_1][:, :3]]
+            robot_spheres_2 = [spheres for spheres in spheres_per_joint[joint_2][:, :3]]
+            distances = cdist(robot_spheres_1, robot_spheres_2)            
+            sum_of_radii = sphere_radius_per_joint[joint_1] + sphere_radius_per_joint[joint_2]
+            if np.any(distances <= sum_of_radii):
+                return True
+    
+        # arm - obstacle collision 
+        obstacles = self.env.obstacles #np.array
+        if obstacles.size > 0:
+            for joint in self.ur_params.ur_links:
+                robot_spheres = [spheres for spheres in spheres_per_joint[joint][:, :3]]
+                distances = cdist(robot_spheres, obstacles)
+                sum_of_radii = sphere_radius_per_joint[joint] + self.env.radius
+                if np.any(distances <= sum_of_radii):
+                    return True      
+
+        self.checked_confs[tupl_conf] = False
+        return False
     
     def local_planner(self, prev_conf ,current_conf) -> bool:
         '''check for collisions between two configurations - return True if trasition is valid
@@ -43,8 +104,13 @@ class Building_Blocks(object):
         @param current_conf - current configuration
         '''
         # TODO 
-        # hint: use self.is_in_collision()
-        
+        # Generate intermediate configurations between prev_conf and current_conf
+        dist_prev_curr = np.linalg.norm(current_conf - prev_conf)
+        num_intermediate_configs = max(int(np.ceil(dist_prev_curr / self.resolution)), MIN_RESOLUTION)
+        intermediate_configs = np.linspace(prev_conf, current_conf, num_intermediate_configs)
+
+        # Check for collisions in intermediate configurations
+        return not any(self.is_in_collision(config) for config in intermediate_configs)
     
     def edge_cost(self, conf1, conf2):
         '''
@@ -53,9 +119,4 @@ class Building_Blocks(object):
         @param conf2 - configuration 2
         '''
         return np.dot(self.cost_weights, np.power(conf1-conf2,2)) ** 0.5
-    
-    
-
-    
-    
     
--- planners.py
+++ planners.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+import math
 from RRTTree import RRTTree
 
 class RRT_STAR(object):
@@ -8,20 +9,57 @@ class RRT_STAR(object):
         self.max_itr = max_itr
         self.bb = bb
         self.tree = RRTTree(bb)
+
+        #our
+        self.edges_start_end = {}
+        self.vertices_with_costs = []
+        self.goal_id = -1
     
     def find_path(self, start_conf, goal_conf, filename):
-        """Implement RRT-STAR"""
+        """Implement RRT-STAR - Return a path as numpy array"""
         
         i = 1
-        time_start = time.time()
-        self.tree.AddVertex(start_conf)
+        start_id = self.add_vertex(start_conf)
 
+        #######
         while i < self.max_itr:
-            pass
-        # TODO
+            i += 1
 
+            rand_conf = self.bb.sample(goal_conf)
+            conf_near_id, conf_near = self.tree.GetNearestVertex(rand_conf)
+            if conf_near_id == self.goal_id:
+                continue
+            conf_new = self.extend(conf_near, rand_conf)            
+            if self.bb.local_planner(conf_near, conf_new):                
+                conf_new_id = -1
+                if np.array_equal(conf_new, goal_conf):
+                    if (self.goal_id == -1):                
+                        conf_new_id = self.add_vertex(conf_new)
+                        dist = self.bb.edge_cost(conf_near, conf_new)
+                        self.add_edge(conf_near_id, conf_new_id, dist)
+                        self.goal_id = conf_new_id
+                    else:
+                        conf_new_id = self.goal_id
+                else:
+                    conf_new_id = self.add_vertex(conf_new)
+                    dist = self.bb.edge_cost(conf_near, conf_new)
+                    self.add_edge(conf_near_id, conf_new_id, dist)
+                
+                k_nearest_ids, _ = self.tree.GetKNN(conf_new, self.get_k_num(i))
+                
+                for potential_father_id in k_nearest_ids:
+                    self.rewire(potential_father_id, conf_new_id)
 
-
+                if conf_new_id != self.goal_id:
+                    for potential_child_id in k_nearest_ids:
+                        self.rewire(conf_new_id, potential_child_id)
+            
+            print(i)
+            
+        #constructing the plan from the goal to the start
+        path, cost = self.get_shortest_path(self.goal_id)
+        
+        return np.array(path), cost
     
     def extend(self, x_near, x_random)-> np.array:
         '''
@@ -30,8 +68,12 @@ class RRT_STAR(object):
         @param x_random - random sampled configuration
         return the extended configuration
         '''
-        # TODO
-       
+        dist = np.linalg.norm(x_random - x_near)
+        if self.max_step_size >= dist:
+            return x_random            
+        x_random = x_near + (((x_random - x_near)/dist) * self.max_step_size)
+
+        return x_random       
     
     def rewire(self, x_potential_parent_id, x_child_id) -> None:
         '''
@@ -41,9 +83,21 @@ class RRT_STAR(object):
         return None
         '''
         # TODO
-        
-           
-
+        if self.is_edge_new(x_child_id, x_potential_parent_id):
+            if self.bb.local_planner(self.tree.vertices[x_potential_parent_id], self.tree.vertices[x_child_id]) :
+                cost_new_edge = self.bb.edge_cost(self.tree.vertices[x_potential_parent_id], self.tree.vertices[x_child_id])
+                cost_father = self.vertices_with_costs[x_potential_parent_id].cost
+                cost_child = self.vertices_with_costs[x_child_id].cost
+                if cost_new_edge + cost_father < cost_child:
+                    #removing the new edge from edges_start_end
+                    if self.tree.edges[x_child_id] in self.edges_start_end:
+                        if x_child_id in self.edges_start_end[self.tree.edges[x_child_id]]:
+                            self.edges_start_end[self.tree.edges[x_child_id]].remove(x_child_id)
+                            if len(self.edges_start_end[self.tree.edges[x_child_id]]) == 0:
+                                del self.edges_start_end[self.tree.edges[x_child_id]]
+                    
+                    self.add_edge(x_potential_parent_id, x_child_id, cost_new_edge)
+                    self.propagate_cost_to_children(x_child_id)      
 
     def get_shortest_path(self, dest):
         '''
@@ -52,7 +106,27 @@ class RRT_STAR(object):
         return the shortest path and the cost
         '''
         # TODO
-        # return path, cost
+        path_goal_to_start = []
+
+        curr_id = dest
+        root_id = self.tree.GetRootID()
+        if curr_id != -1:
+            while (curr_id != root_id):
+                path_goal_to_start.append(self.vertices_with_costs[curr_id].conf)
+                curr_id = self.tree.edges[curr_id]
+                #print("curr id: ", curr_id)
+            path_goal_to_start.append(self.vertices_with_costs[root_id].conf)
+
+        path = np.flipud(path_goal_to_start)       
+
+        cost = 0.0
+        if len(path) == 0:
+            cost = math.inf
+        for i in range(len(path) - 1):
+            cost += self.bb.edge_cost(path[i], path[i + 1])
+            #print("cost: ", cost)
+
+        return path, cost
     
     def get_k_num(self, i):
         '''
@@ -69,3 +143,77 @@ class RRT_STAR(object):
         else:
             k_num = 7
         return k_num
+
+    #our
+    def is_edge_new(self, id_child, id_father):
+        if (id_child == id_father):
+            return False
+        
+        if (id_child in self.tree.edges):
+            if self.tree.edges[id_child] == id_father:
+                return False
+        if (id_child in self.edges_start_end):
+            if id_father in self.edges_start_end[id_child]:
+                return False
+        if (id_father in self.tree.edges):
+            if self.tree.edges[id_father] == id_child:
+                return False
+        if (id_father in self.edges_start_end):
+            if id_child in self.edges_start_end[id_father]:
+                return False
+        
+        return True
+    
+    #our function
+    def propagate_cost_to_children(self, id_father):
+        if id_father in self.edges_start_end:
+            ids_father = []
+            ids_father.append(id_father)
+
+            for id_father in ids_father:
+                vertices_to_change = self.edges_start_end[id_father]
+                for v_id in vertices_to_change:
+                    cost_edge = self.bb.edge_cost(self.tree.vertices[id_father], self.tree.vertices[v_id])
+                    cost_father = self.vertices_with_costs[id_father].cost
+                    self.vertices_with_costs[v_id].set_cost(cost_edge + cost_father)
+                    if v_id in self.edges_start_end and len(self.edges_start_end[v_id]) != 0:
+                        ids_father.append(v_id)
+
+    ##########
+    def add_edge_start_end(self, sid, eid):
+        if sid not in self.edges_start_end:
+            self.edges_start_end[sid] = []
+        self.edges_start_end[sid].append(eid)
+
+    def add_vertex(self, conf):
+        '''
+        Add a state to the tree.
+        @param state state to add to the tree.
+        '''
+        vid = self.tree.AddVertex(conf)
+        self.vertices_with_costs.append(RRTVertex(conf=conf))
+        return vid
+
+    ##########
+    def add_edge(self, sid, eid, edge_cost):
+        '''
+        Adds an edge in the tree.
+        @param sid start state ID
+        @param eid end state ID
+        '''
+        self.tree.AddEdge(sid, eid)            
+        self.add_edge_start_end(sid, eid)
+        self.vertices_with_costs[eid].set_cost(self.vertices_with_costs[sid].cost + edge_cost)
+
+class RRTVertex(object):
+
+    def __init__(self, conf, cost=0):
+
+        self.conf = conf
+        self.cost = cost
+
+    def set_cost(self, cost):
+        '''
+        Set the cost of the vertex.
+        '''
+        self.cost = cost
